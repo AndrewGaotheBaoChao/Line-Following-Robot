203b3aaeb3e434a6190fbd34b291c22d485fd0b3
Andrew Gao <aggaodrew@gmail.com>
2017-11-23 10:03:51 -0500

    Calibrated Servos to Work more Effectively

--- Main.ino
+++ Main.ino
@@ -29,7 +29,7 @@ void setup()
 void loop()
 {
   // read raw sensor values
-  //qtra.read(sensorValues);
+  //  qtra.read(sensorValues);
   // print the sensor values as numbers from 0 to 1023, where 0 means maximum reflectance and 1023 means minimum reflectance
   sensorR = analogRead(A0); // Right servo is connected to analog A0
   sensorL = analogRead(A1); // Left servo is connected to analog A1
@@ -37,23 +37,24 @@ void loop()
   // Pass INC as a parameter to get the distance in inches
   distance = ultrasonic.distanceRead();
   Serial.print(distance);
-  delay(1000);
   
-  if (sensorR <= 100  && sensorL <= 100){ // Move Forward
-    myLservo.write(0);
-    myRservo.write(180);
-    }
-  if (sensorR >= 150){ // When right sensor detects black tape
+  if (distance <= 10){ // Something is in the way, STOP!
     myLservo.write(90);
     myRservo.write(90);
     }
-  if (sensorL >= 150){ // When left sensor detects black tape
-    myLservo.write(180);
-    myRservo.write(180);
-    }
-  if (distance <= 10){ // When robot sees obstacle, it stops
-    myLservo.write(90);
-    myRservo.write(90);
+  else {
+    if (sensorR <= 100  && sensorL <= 100){ // Forward
+      myLservo.write(0);
+      myRservo.write(180);
+      }
+    else if (sensorR >= 200){ // Right Sensor detects black tape
+      myLservo.write(0);
+      myRservo.write(20); //90
+      }
+    else if (sensorL >= 200){ // Left Sensor detects black tape
+      myLservo.write(160); //90
+      myRservo.write(180);
+      }
     }
   // TESTING
   for (unsigned char i = 0; i < NUM_SENSORS; i++)
@@ -62,3 +63,4 @@ void loop()
     Serial.print('\t'); // tab to format the raw data into columns in the Serial monitor
   }
 }
+
